fcd7e76971607b1d5523d5bcdeec7f8f9593c69d
Dag Hansteen <dag.hansteen@gmail.com>
2026-02-16 21:41:19 +0100

    Conflict demo: set serverName to A

--- IRCServer.cs
+++ IRCServer.cs
@@ -22,7 +22,7 @@ namespace Ircd
             listener = new TcpListener(IPAddress.Any, port);
         }
 
-        string serverName = "MyIRCD Testserver 2"; 
+        string serverName = "MyIRCD Testserver A"; 
 
         public async Task StartAsync()
         {
